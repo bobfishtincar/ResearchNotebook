ca604feaad1473c66d7b91dbeee52df192911db3
bobfishtincar <bhavishdinakar@berkeley.edu>
2017-07-21 15:16:14 -0700

    Contains basic Main method.

--- src/bdinakar/researchnotebook/Log.java
+++ src/bdinakar/researchnotebook/Log.java
@@ -5,10 +5,12 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Scanner;
 
 class Log {
 
     private String title;
+    private HashSet<String> initials;
     private HashSet<String> tags;
     private ArrayList<Event> logs;
     private Date logDate;
@@ -17,15 +19,57 @@ class Log {
     Log(String input) {
         title = input;
         logDate = new Date();
+        tags = new HashSet<>();
+        logs = new ArrayList<>();
+        initials = new HashSet<>();
+    }
+
+    void addInitials(String input) {
+        String[] initialsArray = splitLine(input);
+        for (String i: initialsArray) {
+            initials.add(i);
+        }
+    }
+
+    void startLogs(Scanner sc) {
+        while (true) {
+            String line = sc.nextLine();
+            String[] split = splitLine(line);
+
+            if (split.length != 0) {
+                switch (split[0]) {
+
+                    case ("tag"):
+                        addTags(split);
+                        break;
+
+                    case ("qq"):
+                        sc.close();
+                        return;
+
+                    default:
+                        addLog(line);
+                        break;
+                }
+            }
+        }
+    }
+
+    void addTags(String[] input) {
+        for (int i = 1; i < input.length; ++i) {
+            addTags(input[i]);
+        }
     }
 
     void addTags(String input) {
-        tags = new HashSet<>();
-        logs = new ArrayList<>();
-        String[] split = input.trim().split("\\s+");
+        String[] split = splitLine(input);
         for (String i: split) {
             tags.add(i.toLowerCase());
         }
+    }
+
+    private String[] splitLine(String input) {
+        return input.trim().split("\\s+");
     }
 
     void addLog(String input) {
@@ -46,6 +90,9 @@ class Log {
         rtn += (new SimpleDateFormat("MM/dd/yyyy HH:mm")).format(logDate);
         rtn += " ";
         rtn += title;
+        rtn += "\n";
+        rtn += "Initials: ";
+        rtn += initials;
         rtn += "\n";
         rtn += "Tags: ";
         rtn += tags;
--- src/bdinakar/researchnotebook/Main.java
+++ src/bdinakar/researchnotebook/Main.java
@@ -1,8 +1,33 @@
 package bdinakar.researchnotebook;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Title: ");
+        String title = sc.nextLine();
+
+        Log l = new Log(title);
+
+        System.out.println("Enter Initials: ");
+        String initials = sc.nextLine();
+        l.addInitials(initials);
+
+        System.out.println("Enter Tags: ");
+        String tags = sc.nextLine();
+        l.addTags(tags);
+
+        System.out.println("You are now ready to start logging events!");
+        System.out.println("To add a tag, type the word \"tag\" before the tags.");
+        System.out.println("Type \"qq\" to save the log and quit.");
+
+        l.startLogs(sc);
+
+        System.out.println(l);
     }
+
 }
