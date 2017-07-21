5a8548c89a8834f71324d24a1169622d580882fc
bobfishtincar <bhavishdinakar@berkeley.edu>
2017-07-21 14:43:23 -0700

    Contains Log class.

--- src/bdinakar/researchnotebook/Log.java
+++ src/bdinakar/researchnotebook/Log.java
@@ -1,5 +1,7 @@
 package bdinakar.researchnotebook;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -7,14 +9,48 @@ import java.util.HashSet;
 
 class Log {
 
+    private String title;
     private HashSet<String> tags;
     private ArrayList<Event> logs;
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 
     Log(String input) {
+        title = input;
+    }
+
+    void addTags(String input) {
         tags = new HashSet<>();
         logs = new ArrayList<>();
         String[] split = input.trim().split("\\s+");
-        tags.addAll(Arrays.asList(split));
+        for (String i: split) {
+            tags.add(i.toLowerCase());
+        }
+    }
+
+    void addLog(String input) {
+        logs.add(new Event(input));
+    }
+
+    public static void main(String[] args) {
+        Log l = new Log("title");
+        l.addTags("tag1 tag2 tag3");
+        l.addLog("banana");
+        l.addLog("apple");
+        System.out.println(l);
+    }
+
+    @Override
+    public String toString() {
+        String rtn = "";
+        rtn += title;
+        rtn += "\n";
+        rtn += "Tags: ";
+        rtn += tags;
+        rtn += "\n";
+        for (Event e: logs) {
+            rtn += e;
+        }
+        return rtn;
     }
 
     private class Event {
@@ -25,6 +61,11 @@ class Log {
         Event(String eventString) {
             this.eventString = eventString;
             this.eventDate = new Date();
+        }
+
+        @Override
+        public String toString() {
+            return DATE_FORMAT.format(eventDate) + ": " + eventString + "\n";
         }
 
     }
